e3a26349d3f55f8ac8e5ed3a3ae58786a068b5d6
antoda-s <antoda-s@inception.virtualbox.org>
2024-12-20 16:45:36 +0000

    secrets ready

---
+++ Inception_s/srcs/requirements/mariadb/tools/SetupMariaDB.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+MYSQL_DATABASE=$(cat /run/secrets/db_name)
+MYSQL_PASSWORD=$(cat /run/secrets/db_password)
+MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+# echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+# echo "MYSQL_DATABASE: $MYSQL_DATABASE"
+# echo "MYSQL_USER: $MYSQL_USER"
+# echo "MYSQL_PASSWORD: $MYSQL_PASSWORD"
+# echo "MYSQL_ROOT_PASSWORD: $MYSQL_ROOT_PASSWORD"
+# echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+# Initialize database if necessary
+if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
+    echo "Initializing database..."
+
+    # Start the server in the background
+    mysqld_safe --datadir=/var/lib/mysql --user=mysql &
+    sleep 2
+
+    # Setup user and database
+    mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" -h localhost
+    mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" -h localhost
+    mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" -h localhost
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;" -h localhost
+
+    # Shutdown the server estava o primeiro
+    mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown 
+    #kill $(cat /var/run/mysqld/mysqld.pid)
+else
+    echo "Database already initialized"
+fi
+
+# Start the server
+# echo "Check Port"
+# netstat -lntp | grep 3306
+# echo "End Check Port"
+
+# exec mysqld --user=mysql --skip-networking=false #--port=3306
+exec mysqld --bind-address=0.0.0.0 --user=mysql
